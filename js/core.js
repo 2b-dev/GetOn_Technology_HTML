bed8dc4218c6869243e91eda998ef5a1fe94c61f
Nu Chaimongkon <anc.chaimongkon@gmail.com>
2019-12-19 08:27:54 +0700

    add top effect

--- js/core.js
+++ js/core.js
@@ -1,9 +1,28 @@
 $("#switchlang").on("click", function() {
   if ($("#switchlang").is(":checked")) {
-    $('.box-en').addClass('active');
-    $('.box-th').removeClass('active');
+    $(".box-en").addClass("active");
+    $(".box-th").removeClass("active");
   } else {
-    $('.box-en').removeClass('active');
-    $('.box-th').addClass('active');
+    $(".box-en").removeClass("active");
+    $(".box-th").addClass("active");
+  }
+});
+
+var lastScrollTop = 0;
+var fixheight = 180;
+$(window).scroll(function() {
+  var st = $(this).scrollTop();
+  var header = $(".header-geton");
+  if (st > fixheight) {
+    setTimeout(function() {
+      if (st > lastScrollTop) {
+        header.addClass("hide-header");
+      } else {
+        header.removeClass("hide-header");
+      }
+      lastScrollTop = st;
+    }, 100);
+  }else{
+    header.removeClass("hide-header");
   }
 });
